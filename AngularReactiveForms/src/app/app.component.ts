9a45c2fd30a24dd13fb4604f753e425cb7b7a055
Akki0706 <126375876+Akki0706@users.noreply.github.com>
2024-03-29 18:39:24 +0530

    update

--- AngularReactiveForms/src/app/app.component.ts
+++ AngularReactiveForms/src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
+import { NgIf } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ReactiveFormsModule,CommonModule],
+  imports: [RouterOutlet,ReactiveFormsModule,CommonModule,NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -24,19 +27,31 @@ export class AppComponent {
     portfolioLink: new FormControl('')
   })
  })*/
+ registrationform!:FormGroup;
+ get userName(){
+  return this.registrationform.get('userName');
+}
 
+get email(){
+  return this.registrationform.get('email');
+}
  constructor(private fb : FormBuilder){}
+ngOnInit(){
+  this.registrationform = this.fb.group({
+    userName : ['Mohan',[Validators.required,Validators.minLength(3),forbiddenNameValidator(/angular/)]],
+    Password : [''],
+    ConfirmPassword : [''],
+    email:[''],
+    notify:[''],
+   
+    skill : this.fb.group({
+      angularLevel : [''],
+      githubLink : [''],
+      portfolioLink: ['']
+    })
+  } , {validator : PasswordValidator})
+}
 
-registrationform = this.fb.group({
-  userName : ['Mohan'],
-  Password : [''],
-  ConfirmPassword : [''],
-  skill : this.fb.group({
-    angularLevel : [''],
-    githubLink : [''],
-    portfolioLink: ['']
-  })
-})
 
  loadSampleData(){
   this.registrationform.patchValue({
@@ -47,3 +62,18 @@ registrationform = this.fb.group({
  }
 
 }
+function forbiddenNameValidator(forbiddenName:RegExp):ValidatorFn{
+  return (control:AbstractControl):{[key:string]:any }| null => {
+    const forbidden = forbiddenName.test(control.value);
+    return forbidden? {'forbiddenName':{value:control.value}}:null;
+  }
+  }
+  function PasswordValidator(control : AbstractControl) : {[key:string] :boolean}  | null {
+    const password = control.get('Password');
+    const confirmPassword = control.get('ConfirmPassword');
+    if(password?.pristine || confirmPassword?.pristine){
+      return null;
+    }
+    return password && confirmPassword && password.value != confirmPassword.value ? {'misMatch': true} : null;
+  }
+  
