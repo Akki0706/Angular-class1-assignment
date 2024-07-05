994651550e0340b154b2a942b6c663cbd43f8d19
Akki0706 <ankitk54329@gmail.com>
2024-07-05 12:28:17 +0530

    update

--- AngularMaterialUI/MaterialUiPractice/src/app/app.component.ts
+++ AngularMaterialUI/MaterialUiPractice/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { ThousandPipe } from './thousand.pipe'; // Ensure the path is correct
 import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterLink, ThousandPipe, NgFor, NgIf],
+  imports: [RouterLink, ThousandPipe, NgFor, NgIf,RouterOutlet],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
